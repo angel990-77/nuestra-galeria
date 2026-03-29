e62b4b485e4d59d68178df00548e5acf2cc38ef1
angel990-77 <miguelangelmartinezgiraldo32@gmail.com>
2026-03-29 00:32:30 -0500

    Update login credentials and clean up code

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function login() {
   let user = document.getElementById("user").value;
   let pass = document.getElementById("pass").value;
 
-  if (user === "vicky1" && pass === "vicky1") {
+  if (user === "nuestra.galeria" && pass === "vicky.angel") {
     document.querySelector(".login").classList.add("hidden");
     document.querySelector(".app").classList.remove("hidden");
   } else {
